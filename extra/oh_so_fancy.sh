bfd5e57bbf876eb51334150863e825837f6d78f3
jar-lid-jam <jansenjustin.jj@gmail.com>
2014-03-07 17:13:37 -0500

    bash oh_so_fancy.sh ... on ubuntu desktop to see what it does :)

---
+++ extra/oh_so_fancy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+`notify-send "OneDir- You have a new EVENT" "I really do not like the name OneDir \n  But Otherwise, This is ganna be nice \n I used the Thunderbird I icon because I like it. \n Which unless you uninstalled you should have already (Ubuntu desktop 12.04)  \n This will stay up for 5 seconds" -t 5000 -i /usr/share/pixmaps/thunderbird.png`
+
+
