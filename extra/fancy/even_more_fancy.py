d50c359714aebb0155e58fdcdcb75492d2d3fe4e
jar-lid-jam <jansenjustin.jj@gmail.com>
2014-03-08 14:35:07 -0500

    removed unused package

--- extra/fancy/even_more_fancy.py
+++ extra/fancy/even_more_fancy.py
@@ -1,13 +1,17 @@
-import gobject
+__author__ = 'Justin Jansen'
+__status__ = 'Prototype'
+__date__ = '03/08/14'
+
 import gtk
 import appindicator as appi
 import subprocess
 
+
 def main():
     ind = appi.Indicator("OneDIR", "Notify message", appi.CATEGORY_APPLICATION_STATUS)
-    ind.set_status (appi.STATUS_ACTIVE)
+    ind.set_status(appi.STATUS_ACTIVE)
     ind.set_icon("/usr/share/pixmaps/thunderbird.png")
-    ind.set_attention_icon ("indicator-messages-new")
+    ind.set_attention_icon("indicator-messages-new")
     menu = gtk.Menu()
     item = gtk.MenuItem("OneDir Notification such as SYNC'ing")
     menu.append(item)
@@ -15,7 +19,10 @@ def main():
     ind.set_menu(menu)
     gtk.main()
 
+
 if __name__ == '__main__':
-    command = 'notify-send "This is not what I wanted you to notice" "Look up and slightly to the left, for the thunderbird icon. \n I am using that icon as a placeholder for now" -t 5000'
+    command = 'notify-send "This is not what I wanted you to notice" ' \
+              '"Look up and slightly to the left, for the thunderbird icon. \n ' \
+              'I am using that icon as a placeholder for now" -t 5000'
     subprocess.Popen(command, shell=True)
     main()
