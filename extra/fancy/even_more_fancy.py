af343f4dc31285ecf5ce69bce8bf6af43912f7e7
jar-lid-jam <jansenjustin.jj@gmail.com>
2014-03-08 20:55:38 -0500

    changed them a bit, now you might be able to guess what they do

--- extra/fancy/even_more_fancy.py
+++ extra/fancy/even_more_fancy.py
@@ -2,6 +2,7 @@ __author__ = 'Justin Jansen'
 __status__ = 'Prototype'
 __date__ = '03/08/14'
 
+import os
 import gtk
 import appindicator as appi
 import subprocess
@@ -10,7 +11,8 @@ import subprocess
 def main():
     ind = appi.Indicator("OneDIR", "Notify message", appi.CATEGORY_APPLICATION_STATUS)
     ind.set_status(appi.STATUS_ACTIVE)
-    ind.set_icon("/usr/share/pixmaps/thunderbird.png")
+    d = os.getcwd() + "/tiny_strip.png"
+    ind.set_icon(str(d))
     ind.set_attention_icon("indicator-messages-new")
     menu = gtk.Menu()
     item = gtk.MenuItem("OneDir Notification such as SYNC'ing")
@@ -22,7 +24,6 @@ def main():
 
 if __name__ == '__main__':
     command = 'notify-send "This is not what I wanted you to notice" ' \
-              '"Look up and slightly to the left, for the thunderbird icon. \n ' \
-              'I am using that icon as a placeholder for now" -t 5000'
+              '"Look up and slightly to the left, for the new icon." -t 5000'
     subprocess.Popen(command, shell=True)
     main()
