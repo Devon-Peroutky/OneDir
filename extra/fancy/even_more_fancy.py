06b83c6ebdb1b9e3a1d5de36a237f2f79d9b5fa9
jar-lid-jam <jansenjustin.jj@gmail.com>
2014-03-09 15:15:46 -0400

    forgot to upload most import one

--- extra/fancy/even_more_fancy.py
+++ extra/fancy/even_more_fancy.py
@@ -1,29 +1,41 @@
-__author__ = 'Justin Jansen'
-__status__ = 'Prototype'
-__date__ = '03/08/14'
-
-import os
+import gobject
 import gtk
 import appindicator as appi
 import subprocess
+import webbrowser
+import sys
+import os
 
+def wp_launcher(widget):
+    cwd = os.getcwd()
+    url = 'file://' + cwd + '/index.html'
+    webbrowser.open_new(url)
+
+def quit(widget):
+    sys.exit(0)
 
 def main():
     ind = appi.Indicator("OneDIR", "Notify message", appi.CATEGORY_APPLICATION_STATUS)
-    ind.set_status(appi.STATUS_ACTIVE)
-    d = os.getcwd() + "/tiny_strip.png"
-    ind.set_icon(str(d))
-    ind.set_attention_icon("indicator-messages-new")
+    ind.set_status (appi.STATUS_ACTIVE)
+    ind.set_icon("/home/justin/tiny_strip.png")
+    ind.set_attention_icon ("indicator-messages-new")
     menu = gtk.Menu()
-    item = gtk.MenuItem("OneDir Notification such as SYNC'ing")
-    menu.append(item)
-    item.show()
+    title = gtk.MenuItem("OneDir Notification: (click bellow)")
+    notif = gtk.MenuItem("A friend invited you to share a folder")
+    q = gtk.MenuItem("quit")
+    notif.connect("activate", wp_launcher)
+    q.connect("activate", quit)
+    menu.append(title)
+    menu.append(notif)
+    menu.append(q)
+    title.show()
+    notif.show()
+    q.show()
     ind.set_menu(menu)
     gtk.main()
 
-
 if __name__ == '__main__':
-    command = 'notify-send "This is not what I wanted you to notice" ' \
-              '"Look up and slightly to the left, for the new icon." -t 5000'
+    command = 'notify-send "This is not what I wanted you to notice" "Look up and slightly to the left, for the thunderbird icon. \n I am using that icon as a placeholder for now" -t 5000'
     subprocess.Popen(command, shell=True)
     main()
+
