47de8199e2ea1b3cf11abb62aeb3beb72d8b4f14
Rupali Vohra <rupali2vohra@gmail.com>
2014-04-20 15:40:01 -0400

    Making sure I can push things to the repo.

---
+++ Listener/pyinotify/user_signup.py
@@ -0,0 +1,6 @@
+__author__ = 'rupali'
+# assume you are the admin
+# prompt user for username and password
+# call "add_user" command from client.py to create a new user
+# note to self: never delete the admin
+
