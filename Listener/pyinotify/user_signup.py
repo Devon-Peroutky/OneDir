3c858b51265c2fa47fad9c66916028c48990c84a
Rupali Vohra <rupali2vohra@gmail.com>
2014-04-20 17:57:10 -0400

    nothing useful here yet; just trying to pull down runner.py

--- Listener/pyinotify/user_signup.py
+++ Listener/pyinotify/user_signup.py
@@ -3,4 +3,17 @@ __author__ = 'rupali'
 # prompt user for username and password
 # call "add_user" command from client.py to create a new user
 # note to self: never delete the admin
+from client import *
 
+def __init__(self):
+    print "nothing yet"
+    self.o =
+    self.client = new OneDirAdminClient()
+
+def main(self):
+    print "Welcome new user! Enter in your desired username:"
+
+def check_user_available(self, un):
+
+
+if __name__ = "__main__":
