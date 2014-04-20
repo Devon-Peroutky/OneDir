e9e953ad8eebeaeb165fea9391666f3e36778b1c
Rupali Vohra <rupali2vohra@gmail.com>
2014-04-20 18:14:21 -0400

    Some progress on creating a user; working on it again after dinner, but I wanted to push stuff before Justin refactored just in case I forget later.

--- Listener/pyinotify/user_signup.py
+++ Listener/pyinotify/user_signup.py
@@ -4,16 +4,29 @@ __author__ = 'rupali'
 # call "add_user" command from client.py to create a new user
 # note to self: never delete the admin
 from client import *
+import sys
 
 def __init__(self):
-    print "nothing yet"
-    self.o =
-    self.client = new OneDirAdminClient()
+    #self.o = OneDirFtpClient(socket.gethostbyname(socket.gethostname()), "admin", "admin", os.getcwd())
+    self.client = OneDirAdminClient(socket.gethostbyname(socket.gethostname()), "admin", "admin", os.getcwd())
 
 def main(self):
-    print "Welcome new user! Enter in your desired username:"
+    username = raw_input("Welcome new user! Enter in your desired username:")
+    check = check_user_available(username)
+    while not check:
+        username = raw_input("Sorry, that username is taken. Please enter another username.")
+        check = check_user_available(username)
+    pw = raw_input("Username available! Please enter in a desired password:")
+    pw_check = raw_input("Please re-enter your password:")
+    while not pw == pw_check:
+        print "Passwords do not match. Please re-enter a password."
 
-def check_user_available(self, un):
 
+def check_user_available(self, username):
+    users = self.client.get_user_list
+    if username not in users:
+        return True
+    else:
+        return False
 
 if __name__ = "__main__":
