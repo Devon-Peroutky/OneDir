570be1494022b74b55d5c4ec9cdf26877bf3ca58
jar-lid-jam <jansenjustin.jj@gmail.com>
2014-04-20 22:05:55 -0400

    install file, does not check requirements yet

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='One-Dir',
+    version='0.0.9',
+    packages=['', 'OneDirServer', 'OneDirListener'],
+    url='',
+    license='',
+    author='Justin Jansen, Rupali Vohra, Devon Peroutky, Jennifer Lu',
+    author_email='jl3yr@virginia.edu, jj3bn@virginia.edu, dsp3eh@virginia.edu, rv5rr@virginia.edu, ',
+    description=''
+)
