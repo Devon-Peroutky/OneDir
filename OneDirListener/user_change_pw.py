4825b8a5c95b561ab6c61589d1cacd16b2bd7020
Rupali Vohra <rupali2vohra@gmail.com>
2014-04-22 22:33:04 -0400

    minor updates on the way to big change.

--- OneDirListener/user_change_pw.py
+++ OneDirListener/user_change_pw.py
@@ -1,20 +1,21 @@
 __author__ = 'rupali'
 from client import *
+from getpass import getpass
 
-
-def main(self):
+#use set_password from client.py
+def main(self, ip, username, password, root_dir):
     """
     Prompts a current user through changing their password
     """
-    client = OneDirAdminClient(socket.gethostbyname(socket.gethostname()), "admin", "admin", os.getcwd())
-    username = raw_input("Please enter in your username:") #can be removed if there's a get_current_user() method somewhere I didn't see
-    check = check_user(username)
-    while not check:
-        username = raw_input("Entered username not valid. Please enter a valid username:")
-    current_pw = "dummy" #need to access database to get user's current pw
-    verify_pw = raw_input("Please enter in your current password:")
-    while not current_pw == verify_pw:
-        verify_pw = raw_input("Incorrect password entry. Please enter current valid password:")
+    #client = OneDirFtpClient(socket.gethostbyname(ip, username, password, root_dir))
+    #username = raw_input("Please enter in your username:") #can be removed if there's a get_current_user() method somewhere I didn't see
+    #check = check_user(username)
+    #while not check:
+    #    username = raw_input("Entered username not valid. Please enter a valid username:")
+    #current_pw = "dummy" #need to access database to get user's current pw
+    verify_pw = getpass("Please enter in your current password:")
+    #while not current_pw == verify_pw:
+    #    verify_pw = getpass("Incorrect password entry. Please enter current valid password:")
     new_pw = raw_input("Enter in your desired new password:")
     verify_new_pw = raw_input("Re-enter your desired new password:")
     while not new_pw == verify_new_pw:
@@ -22,6 +23,7 @@ def main(self):
         new_pw = raw_input("Enter in your desired new password:")
         verify_new_pw = raw_input("Re-enter your desired new password:")
     client.change_user_password(username, new_pw)
+
 
 def check_user(self, username):
     """
--- OneDirListener/user_signup.py
+++ OneDirListener/user_signup.py
@@ -4,23 +4,25 @@ __author__ = 'rupali'
 # call "add_user" command from client.py to create a new user
 # note to self: never delete the admin
 from client import *
+from getpass import getpass
 
 
-def main(self):
+def main(self): #connect with client.py after Justin pushes; use OneDirAdminClient to create new user with temp pw, then proceed to change pw
+    #OneDirNoAuthClient - coming soon! (requires IP and port number); request username. If accepted, temp pw will be issued. If username is accepted, error thrown.
     """
     Prompts a new user through the creation of a non-admin account.
     """
-    client = OneDirAdminClient(socket.gethostbyname(socket.gethostname()), "admin", "admin", os.getcwd())
+    #client = OneDirAdminClient(socket.gethostbyname(socket.gethostname()), "admin", "admin", os.getcwd())
     username = raw_input("Welcome new user! Enter in your desired username:")
     check = check_user_available(username)
     while not check:
         username = raw_input("Sorry, that username is taken. Please enter another username.")
         check = check_user_available(username)
-    pw = raw_input("Username available! Please enter in a desired password:")
-    pw_check = raw_input("Please re-enter your password:")
+    pw = getpass("Username available! Please enter in a desired password:")
+    pw_check = getpass("Please re-enter your password:")
     while not pw == pw_check:
-        pw = raw_input("Passwords do not match. Please enter a desired password:")
-        pw_check = raw_input("Please re-enter your password:")
+        pw = getpass("Passwords do not match. Please enter a desired password:")
+        pw_check = getpass("Please re-enter your password:")
     self.client.user_add(username, pw, False)
 
 
